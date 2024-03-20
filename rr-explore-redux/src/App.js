ef03b903273fb6977bfa0a1cdd32437d9ef8b874
keylat160 <keylat160@gmail.com>
2024-03-20 00:26:51 -0700

    adding content

---
+++ rr-explore-redux/src/App.js
@@ -0,0 +1,15 @@
+import Counter from '../../rr-explore-redux/src/Counter'
+import Todo from '../../rr-explore-redux/src/Todo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
+}
+
+
+export default App;
